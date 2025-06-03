092ea846fa16cfed3e26b9fcef86b7c804e844c0
sanuthmee1578 <sanuthmee0625@gmail.com>
2025-06-03 22:46:47 +0530

    Initial push after reconnecting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/navbar/NavBar';
+import Home from './pages/Home';
+// import UniqueSection from './pages/UniqueSection';
+// import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+           <img src='' alt= 'logo' className='navbar-logo'></img>
+            <ul className='navbar-links'>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#products'>Products</a></li>
+                <li><a href='#contact'>Contact</a></li>
+                <li><a href='#policy'>Policy</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
