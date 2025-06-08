779abe959aa58f2d709746f1a8d60f94e1590020
Oshan <oshankodagoda200667@gmail.com>
2025-06-08 23:54:52 +0530

    make components for the why eat cinnamon section, and made half of it

---
+++ src/components/cinnSOGud/benifits.jsx
@@ -0,0 +1,14 @@
+import "./benifits.css"
+export default function Benfits(){
+    return(
+        <>
+    <div className="support-card">
+        <h2 className="card-title">First-class Support</h2>
+        <p className="card-description">
+            Prompt and seamless service, provided by core competency experts.
+        </p>
+    </div>
+        </>
+    )
+}
+
---
+++ src/components/cinnSOGud/cinnSoGud.jsx
@@ -0,0 +1,15 @@
+import Benfits from "./benifits.jsx";
+import "./cinnSoGud.css"
+
+export default function CinSoGud(){
+    return(
+        
+    <div className="banner">
+
+        <div className="banner-text">Why cinnamon so good</div>
+        <div className="benfits-wrapper">
+        <Benfits></Benfits>
+    </div>
+        </div>
+    )
+}
