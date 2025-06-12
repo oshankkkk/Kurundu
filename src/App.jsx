3a0557c1a256b9f82e7e27081ca1d07f8973b312
Oshan <oshankodagoda200667@gmail.com>
2025-06-12 17:28:51 +0530

    1st component merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import './App.css';
 import WhyCinnamon from './components/Importance_section/WhyCinnamon';
-// import CinSoGud from './components/cinnSOGud/cinnSoGud';
+import CinSoGud from './components/cinnSOGud/cinnSoGud';
 import Navbar from './components/navbar/NavBar';
 import Footer from './pages/Footer';
-// import ProductsSection from './components/products/products';
+import ProductsSection from './components/products/products';
 import Home from './pages/Home';
+import Scroller from './components/scroller/scroller';
 
 function App() {
   return (
     <>
-    {/* <ProductsSection></ProductsSection>  */}
-    {/* <CinSoGud></CinSoGud> */}
  <Navbar />
       <Home />
+    <Scroller></Scroller>
+    <CinSoGud></CinSoGud>
+    <ProductsSection></ProductsSection> 
       <WhyCinnamon/>
       <Footer/>
     </>
