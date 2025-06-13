56c55d6b750cb59b07e3bb6ebe51648378a24681
sanuthmee1578 <sanuthmee0625@gmail.com>
2025-06-13 13:55:36 +0530

    icons folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import Navbar from './components/navbar/NavBar';
 import Footer from './pages/Footer';
 import ProductsSection from './components/products/products';
 import Home from './pages/Home';
-import Scroller from './components/scroller/scroller';
 
 function App() {
   return (
     <>
  <Navbar />
       <Home />
-    <Scroller></Scroller>
     <CinSoGud></CinSoGud>
     <ProductsSection></ProductsSection> 
       <WhyCinnamon/>
