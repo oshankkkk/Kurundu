53318fd0047eee82f40bd330643a040f32fff1ac
Oshan <oshankodagoda200667@gmail.com>
2025-06-15 21:00:35 +0530

    add responsive degsin for hero section

--- src/components/heroSection/heroSection.jsx
+++ src/components/heroSection/heroSection.jsx
@@ -7,7 +7,11 @@ return(
       <div className="hero-text">
         <h1>ANCIENT FLAVOUR,<br/>MODERN LIVING</h1>
         <p>Experience the warmth and richness of pure Ceylon cinnamon.</p>
+        <div className='action-but'>
         <button className="explore-btn">Explore Now!</button>
+        <button className="explore-btn">Contact Us</button>
+
+        </div>
       </div>
       <div className="hero-image">
         <img src={cinnamonImg} alt="Ceylon Cinnamon Pack" />
