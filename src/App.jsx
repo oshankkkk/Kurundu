48b9309d908c292174b50191d274db7646614812
Oshan <oshankodagoda200667@gmail.com>
2025-06-15 20:57:45 +0530

    setup react router for page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/NavBar';
 import Footer from './components/footer/Footer';
 import Home from './pages/Home';
+import Policy from './pages/Policy';
 
 function App() {
   return (
-    <>
- <Navbar />
-      <Home />
-  
-      <Footer/>
-    </>
+    <Router >
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/policy" element={<Policy />} />
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
 
