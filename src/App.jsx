a080a5b01bdf8f9041acf889c90f869dea8775d1
Oshan <oshankodagoda200667@gmail.com>
2025-06-16 22:26:04 +0530

    craate cart state var and distribute using react context and store value in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/NavBar';
-import Footer from './components/footer/Footer';
-import Home from './pages/Home';
-import Policy from './pages/Policy';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/NavBar";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Policy from "./pages/Policy";
+import OrderForm from "./components/orderForm/orderForm";
+import { Checkout } from "./pages/checkout";
+import { createContext, useState } from "react";
+export const cartContext = createContext();
 function App() {
+  let [cartState,setCartState]=useState(
+JSON.parse(localStorage.getItem("cart") || "[]"),
+  )
   return (
-    <Router basename='/Kurundu' >
+    <cartContext.Provider value={{cartState,setCartState}}>
+
+    <Router basename="/Kurundu">
       <Navbar />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/policy" element={<Policy />} />
+        <Route path="/checkout" element={<Checkout></Checkout>} />
       </Routes>
 
       <Footer />
     </Router>
+    </cartContext.Provider>
   );
 }
 
