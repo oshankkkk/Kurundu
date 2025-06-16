a576f87253d57f0013d28aa3ec3b7e68c914b337
Oshan <oshankodagoda200667@gmail.com>
2025-06-16 22:26:57 +0530

    create add to cart button on product

--- src/components/products/product.jsx
+++ src/components/products/product.jsx
@@ -6,11 +6,18 @@ function Product({ product }) {
         <div className="product-image-wrapper">
           <img className="product-img" src={product.imageURL} alt="img" />
         </div>
+        <div className="product-price-wrapper">
+
         <h2 className="product-name">{product.name}</h2>
+        <h3 className="product-price">{product.price}</h3>
+        </div>
         <div className="product-info">
           <h2 className="product-heading">{product.name}</h2>
           <p className="product-description">{product.description}</p>
         </div>
+        <button>
+          add to cart
+        </button>
       </div>
     </>
   );
