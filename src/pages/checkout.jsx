12ac7c9d162d83725d7cab53ebf4a1db71b8063e
Oshan <oshankodagoda200667@gmail.com>
2025-06-16 22:27:22 +0530

    assemble chackout page

---
+++ src/pages/checkout.jsx
@@ -0,0 +1,13 @@
+import OrderForm from "../components/orderForm/orderForm";
+import "../styles/checkoutPage.css" 
+export function Checkout(){
+return(
+    <div className="checkoutpage">
+<OrderForm>
+</OrderForm>
+<div>
+    cartsummery
+</div>
+    </div>
+)
+}
